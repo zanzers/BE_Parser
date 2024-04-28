d11596442b4cb1975bc9d7e49f9908c3110724e1
zanzers <nowellsaavedra999@gmail.com>
2024-04-28 21:05:52 +0800

    Nothing

--- Parser/main.js
+++ Parser/main.js
@@ -9,7 +9,7 @@ const startTime = performance.now();
 // A+A'B+B'
 // A+A'B
 
-const inputs = "A+A'B";
+const inputs = "A+A'B+B'";
 console.log("USER INPUT: ",inputs);
 // const inputv = executioner();
 
